995e06f6214050e80500075635b8a626c1a7658b
Jon Andoni Galdos <jonandonigv@gmail.com>
2021-10-03 21:39:10 +0200

    add routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,21 +3,27 @@ import express from 'express';
 import { Router } from 'express';
 import { body } from 'express-validator';
 
-import User from '../models/user';
+import {User} from '../models/user';
 import * as authController from '../controllers/user';
 
 const router = Router();
 
 router.post('/signin', [
-    body('email').isEmail().withMessage('Please enter a valid Email').custom((value, {req}) => {
+    body('email').isEmail().withMessage('Please enter a valid Email')
+    /* .custom((value: string, {req}) => {
         return User.findOne({email:value}).then((userDoc: any) => {
             if (userDoc) {
                 return Promise.reject('E-mail already exist!')
             }
         });
-    }).normalizeEmail(),
+    }) */
+    .normalizeEmail(),
     body('password').trim().isLength({min: 6}),
     body('name').trim().not().isEmpty()
 ], authController.signIn);
+
+router.post('/signup', authController.signUp);
+
+router.delete('/deleteAccount', authController.deleteAccount);
 
 export {router};
