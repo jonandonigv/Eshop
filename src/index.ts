436fe45bfc2893cf99fec39d9034fbf1e6059d5d
Jon Andoni Galdos <jonandonigv@gmail.com>
2021-10-03 20:56:49 +0200

    database connection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import {Request, Response, NextFunction} from 'express';
+import { Sequelize } from 'sequelize';
+
+// My imports
 import {router as authRoutes} from './routes/auth';
 
 const app = express();
@@ -21,14 +24,30 @@ app.use((req, res, next) => {
 
 app.use((error: Error, req:Request, res:Response, next:NextFunction) => {
     console.log(error);
-    const status = error.statusCode || 500;
+    /* const status = error.statusCode || 500;
     const message = error.message;
     const data = error.data;
-    res.status(status).json({message: message, data:data})
+    res.status(status).json({message: message, data:data}) */
 })
 
 // Routes
 app.use('/auth', authRoutes);
+
+// db conection
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'postgres',
+});
+
+try {
+    sequelize.authenticate();
+    console.log('Connection has been established succesfully!');
+    app.listen(port, () => {
+        console.log('The server is running in port 3000');
+    });
+} catch (error) {
+    console.log('Unable to connect to databas: ', error);
+};
 
 app.listen(port, () => {
     console.log('The server is running in port 3000');
