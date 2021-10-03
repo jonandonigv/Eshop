c18a46dc1fc0942e6e90f7f826ea646b672af208
Jon Andoni Galdos <jonandonigv@gmail.com>
2021-10-03 18:53:04 +0200

    User actions

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -3,7 +3,15 @@ import 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import {validationResult} from 'express-validator'
 
-export const signIn = async ((req: Request, res: Response, next: NextFunction) => {
+export const signIn = async (req: Request, res: Response, next: NextFunction) => {
     // pass
-})
+}
+
+export const signUp = async (req: Request, res: Response, next: NextFunction) => {
+    // pass
+}
+
+export const deleteAccount = async (req: Request, res: Response, next: NextFunction) => {
+    // pass
+}
 
