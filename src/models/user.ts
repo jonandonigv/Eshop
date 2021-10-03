b8f886489ddb74101862bf5d9385aa2cc01425c6
Jon Andoni Galdos <jonandonigv@gmail.com>
2021-10-03 18:51:46 +0200

    models

---
+++ src/models/user.ts
@@ -0,0 +1,56 @@
+import {
+    Sequelize,
+    Model,
+    ModelDefined,
+    DataTypes,
+    HasManyGetAssociationsMixin,
+    HasManyAddAssociationMixin,
+    HasManyHasAssociationMixin,
+    Association,
+    HasManyCountAssociationsMixin,
+    HasManyCreateAssociationMixin,
+    Optional,
+} from 'sequelize';
+
+import Cart from '../models/cart';
+
+const sequelize = new Sequelize('shop', 'root', 'password', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+// These are all the attributes in the User model
+interface UserAttributes {
+    id: number,
+    name: string,
+    email: string,
+    password: string,
+}
+
+// Some attributes are optionl in `User.build` and `User.create` calls.
+interface UserCreationAttributes extends Optional<UserAttributes, "id"> {}
+
+export class User extends Model<UserAttributes, UserCreationAttributes>
+    implements UserAttributes {
+        public id!: number; // Note that the `null assertion` `!` is required in strict mode.
+        public name!: string;
+        public email!: string;
+        public password!: string;
+
+        // Timestamps!
+        public readonly createdAt!: Date;
+        public readonly updatedAt!: Date;
+
+        // Since TS cannot determine model association at compile time
+        // we have to declare them here purely virtually
+        // these will not exist until `Model.init` was called.
+        public getCart!: HasManyGetAssociationsMixin<Cart>; // Note the null assertions!
+
+        // You can also pre-declare possible inclusions, these will only be populated if you
+        //actively include a relation
+        public readonly cart?: Cart[]; // Note this is optional since it's onlu populated when explicitly requested in code.
+
+        public static associations: {
+            cart: Association<User, Cart>;
+        };
+    }
