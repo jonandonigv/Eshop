cdce791359e683e246aaf25d4a3e070efb2f45d3
Jon Andoni Galdos <jonandonigv@gmail.com>
2021-10-07 22:46:26 +0200

    comment

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === "development") {
 }
 
 /* 
-    * Start Express server.
+    * Starts Express server.
 */
 const server = app.listen(app.get("port"), () => {
     console.log(
