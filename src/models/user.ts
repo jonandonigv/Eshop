2b71f6f5c48e8534de089a61fb66f165874c6ef2
Jon Andoni Galdos <jonandonigv@gmail.com>
2021-10-09 20:26:35 +0200

    user.save function

--- src/models/user.ts
+++ src/models/user.ts
@@ -52,3 +52,20 @@ const userSchema = new mongoose.Schema<UserDocument>(
   },
   { timestamps: true },
 );
+
+/* 
+  * Password hash middleware
+*/
+userSchema.pre("save", function save(next) {
+  const user = this as UserDocument;
+  if (!user.isModified("password")) {return next(); }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err); }
+    bcrypt.hash(user.password, salt, undefined, (err: mongoose.Error, hash) => {
+      if (err) {return next(err); }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
