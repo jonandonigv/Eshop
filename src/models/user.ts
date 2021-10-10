3b88bbeffa677a85d22175ac7aa3fee505efb7ce
Jon Andoni Galdos <jonandonigv@gmail.com>
2021-10-10 21:42:15 +0200

    User helper methods

--- src/models/user.ts
+++ src/models/user.ts
@@ -69,3 +69,23 @@ userSchema.pre("save", function save(next) {
   });
 });
 
+const comparePassword: comparePasswordFunction = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err: mongoose.Error, isMatch: boolean) => {
+    cb(err, isMatch);
+  });
+}
+
+userSchema.methods.comparePassword = comparePassword;
+
+/* 
+  * Helper method for getting user's gravatar
+*/
+userSchema.methods.gravatar = function (size: number = 200) {
+  if (!this.email) {
+    return `https://gravatar.com/avatar/?s=${size}&d=retro`;
+  }
+  const md5 = crypto.createHash("md5").update(this.email).digest("hex");
+  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
+};
+
+export const User = mongoose.model<UserDocument>("User", userSchema);
