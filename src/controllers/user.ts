6e19e6e45c5bdde1ca30100f0b3b92d6298bf60c
Jon Andoni <jonandonigv@gmail.com>
2021-11-02 17:51:51 +0100

    A lot of thing happened

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -16,29 +16,6 @@ import { json } from 'stream/consumers';
     * @route POST /login
 */
 export const postLogin = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    await check('email', 'Email is not valid').isEmail().run(req);
-    await check('password', 'Password cannot be blank').isLength({min: 1}).run(req);
-    await body('email').normalizeEmail({ gmail_remove_dots: false }).run(req);
-
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        req.flash('errors', errors.array());
-        res.status(400).json(errors).send({ message: 'Error' });
-    }
-
-    passport.authenticate("local", (err: Error, user: UserDocument, info: IVerifyOptions) => {
-        if (err) { return next(err); }
-        if (!user) {
-            req.flash("errors", {msg: info.message});
-            return res.status(400).send({msg: info.message});
-        }
-        req.logIn(user, (err) => {
-            if (err) { return next(err); }
-            req.flash("success", {msg: "Success! You are logged in."});
-            res.status(200).send({msg: "Success"});
-        });
-    })(req, res, next)
 };
 
 /* 
@@ -46,35 +23,6 @@ export const postLogin = async (req: Request, res: Response, next: NextFunction)
     * @route POST /signup
 */
 export const postSignup = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    await check("email", "Email is not valid").isEmail().run(req);
-    await check("password", "Password must be at least 6 character long").isLength({min: 6}).run(req);
-    await check("confirmPassword", "Password does not match").equals(req.body.password).run(req);
-    await body("email").normalizeEmail({ gmail_remove_dots: false }).run(req);
-
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        res.status(400).send({errors: errors});
-    }
-
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password
-    });
-
-    User.findOne({ email: req.body.email }, (err: NativeError, existingUser: UserDocument) => {
-        if (err) { return next(err); }
-        if (existingUser) {
-            res.status(401).send({msg: "Account with that email address already exists"});
-        }
-        user.save((err) => {
-            if (err) { return next(err); }
-            req.logIn(user, (err) => {
-                if (err) { return next(err); }
-                res.status(201).send({msg: "Account created correctly"});
-            });
-        });
-    });
 };
 
 /* 
