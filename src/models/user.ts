0dfc7b29c940aab1efe1a4f8da300175c8753438
Jon Andoni <jonandonigv@gmail.com>
2021-11-09 01:59:16 +0100

    Save method modified to encrypt the pass on save

--- src/models/user.ts
+++ src/models/user.ts
@@ -58,18 +58,18 @@ const userSchema = new mongoose.Schema<UserDocument>(
 /* 
   * Password hash middleware
 */
-/* userSchema.pre("save", function save(next) {
+userSchema.pre("save", function save(next) {
   const user = this as UserDocument;
   if (!user.isModified("password")) {return next(); }
   bcrypt.genSalt(10, (err, salt) => {
     if (err) { return next(err); }
-    bcrypt.hash(user.password, salt, undefined, (err: mongoose.Error, hash) => {
+    bcrypt.hash(user.password, salt,(err: mongoose.Error, hash: any) => {
       if (err) {return next(err); }
       user.password = hash;
       next();
     });
   });
-}); */
+});
 
 const comparePassword: comparePasswordFunction = function (this: any, candidatePassword, cb) {
   bcrypt.compare(candidatePassword, this.password, (err: mongoose.Error, isMatch: boolean) => {
