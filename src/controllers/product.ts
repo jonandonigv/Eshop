3bcd4f1c367f2f85f05c5cf49ff60b672136f941
Jon Andoni <jonandonigv@gmail.com>
2021-11-18 20:10:59 +0100

    Products enpoints declared

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,24 +1,36 @@
-import async from 'async';
 import crypto from 'crypto';
 import nodemailer from 'nodemailer';
-import passport from 'passport';
 import { Product, ProductDocument } from '../models/product';
 import { Request, Response, NextFunction } from 'express';
-import { IVerifyOptions } from 'passport-local';
-import { WriteError } from 'mongodb';
-import { body, check, validationResult } from 'express-validator';
-import '../config/default';
-import { CallbackError, NativeError } from 'mongoose';
 
 /* 
     * Get all products
     * @route GET /products
 */
-export const getProducts = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    // Get all the products
+export const getAllProducts = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    // Get all products
+};
+
+/* 
+    * Get product detail
+    * @route GET /products
+*/
+export const getProductDetail = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    // Get all products
 };
 
 /* 
     * Create new Product
     * @route POST /createProduct
 */
+export const postCreateOneProduct = async () => {
+    // TODO: Create one product from form.
+};
+
+/* 
+    * CreateProductFromCSV
+    * @route POST /createProductFromCSV
+*/
+export const postCreateProductFromCSV = async () => {
+
+}
