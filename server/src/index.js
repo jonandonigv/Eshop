34d03d35cc61ea0bb63e43baf82255edafdb1de3
jonandonigv <jonandonigv@gmail.com>
2022-02-16 13:25:12 +0100

    Basic server api setup

---
+++ server/src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+//LOCAL IMPORTS
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+// ROUTES
+
+app.listen(process.env.PORT, () => {
+  mongoose
+    .connect(process.env.MONGODB)
+    .then((result) => {
+      console.log("[SERVER]: DB CONNECTION STABLISHED.");
+      console.log("[SERVER]: API RUNNING AT PORT: " + process.env.PORT);
+    })
+    .catch((err) => console.log(err));
+});
