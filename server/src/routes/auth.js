74b5b17d983ed58d8e13d647c31b71e70f1ec7df
jonandonigv <jonandonigv@gmail.com>
2022-02-23 16:39:23 +0100

    Register endpoint

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,13 +1,28 @@
-const router = require('express').Router();
-const User = require('../models/User');
-const CryptoJs = require('crypto-js');
-const jwt = require('jsonwebtoken');
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJs = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 // REGISTER
-router.post('/register', async(req, res, next) => {});
+router.post("/register", async (req, res, next) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJs.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString(),
+  });
 
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // LOGIN
-router.post('/login', async (req, res, next) => {});
+router.post("/login", async (req, res, next) => {});
 
 module.exports = router;
