9f0493b37ce2ea31a01ab4443c7749da685d4b97
jonandonigv <jonandonigv@gmail.com>
2022-03-01 16:57:48 +0100

    Login and Register

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -23,6 +23,38 @@ router.post("/register", async (req, res, next) => {
 });
 
 // LOGIN
-router.post("/login", async (req, res, next) => {});
+router.post("/login", async (req, res, next) => {
+  try {
+    const user = await User.findOne({ userName: req.body.username });
+
+    !user && res.status(401).json("Wrong username");
+
+    const hashedPassword = CryptoJs.AES.decrypt(
+      user.password,
+      process.env.SECRET_KEY
+    );
+
+    const originalPassword = hashedPassword.toString(CryptoJs.enc.Utf8);
+
+    const inputPassword = req.body.password;
+
+    originalPassword != inputPassword && res.status(401).json("Wrong password");
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.SECRET_KEY,
+      { expiresIn: "3d" }
+    );
+
+    const {password, ...info} = user._doc;
+    res.status(200).json({...others, accessToken});
+
+  } catch (error) {
+      res.status(500).json(error);
+  }
+});
 
 module.exports = router;
