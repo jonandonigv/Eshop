779cf904d499812c245b8de4d491a639ed5b4ca7
jonandonigv <jonandonigv@gmail.com>
2022-03-01 17:05:10 +0100

    Declared product endpoints

---
+++ server/src/routes/product.js
@@ -0,0 +1,28 @@
+const Product = require('../models/Product');
+const {
+    verifyToken,
+    verifyTokenAndAdmin,
+    verifyTokenAndAuthorization
+} = require('../utils/isAuth');
+
+const router = require('express').Router();
+
+// CREATE
+
+router.post('/', verifyTokenAndAdmin, async (req, res, next) => {});
+
+// UPDATE
+
+router.put('/:id', verifyTokenAndAdmin, async (req, res, next) => {});
+
+// DELETE
+
+router.delete('/:id', verifyTokenAndAdmin, async (req, res, next) => {});
+
+// GET PRODUCT
+
+router.get('/find/:id', async (req, res, next) => {});
+
+// GET ALL PRODUCTS
+
+router.get('/', async (req, res, next) => {});
