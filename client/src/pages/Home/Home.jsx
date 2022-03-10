ec0953313c083137b898eb34d288e1bee0d8ea80
jonandonigv <jonandonigv@gmail.com>
2022-03-10 19:35:58 +0100

    stuff

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Announcement } from '../../components/Announcement/Announcement';
 import { Navbar } from '../../components/Navbar/Navbar';
+import Slider from '../../components/Slider/Slider';
 
 export const Home = () => {
   return <div>
       <Navbar />
       <Announcement />
+      <Slider />
   </div>;
 };
