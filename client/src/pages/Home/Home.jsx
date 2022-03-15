fd281f94140982f1d787007362cfbde50da768b6
jonandonigv <jonandonigv@gmail.com>
2022-03-15 21:59:43 +0100

    Products

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import { Announcement } from '../../components/Announcement/Announcement';
-import Categories from '../../components/Categories/Categories';
-import { Navbar } from '../../components/Navbar/Navbar';
-import Slider from '../../components/Slider/Slider';
+import React from "react";
+import { Announcement } from "../../components/Announcement/Announcement";
+import Categories from "../../components/Categories/Categories";
+import { Navbar } from "../../components/Navbar/Navbar";
+import Products from "../../components/Products/Products";
+import Slider from "../../components/Slider/Slider";
 
 export const Home = () => {
-  return <div>
+  return (
+    <div>
       <Navbar />
       <Announcement />
       <Slider />
       <Categories />
-  </div>;
+      <Products />
+    </div>
+  );
 };
