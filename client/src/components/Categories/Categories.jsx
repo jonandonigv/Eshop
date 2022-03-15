d3471145c8eae74bab4619b03d2a4bb87d412f1e
jonandonigv <jonandonigv@gmail.com>
2022-03-15 20:02:40 +0100

    Categories fixed

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -4,16 +4,16 @@ import { categories } from "../../data";
 import CategoryItem from "../CategoryItem/CategoryItem";
 
 const Container = styled.div`
-  display: flex;
-  padding: 20px;
-  justify-content: space-between;
+display: flex;
+padding: 20px;
+justify-content: space-between;
 `;
 
 const Categories = () => {
   return (
     <Container>
       {categories.map((item) => (
-        <CategoryItem />
+        <CategoryItem item={item}/>
       ))}
     </Container>
   );
