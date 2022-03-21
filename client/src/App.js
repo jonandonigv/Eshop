ea01b2863bdd1ab88f4937fb87f2bf87daddbb3f
jonandonigv <jonandonigv@gmail.com>
2022-03-21 22:32:21 +0100

    Register import

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import { Home } from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Home />
+        <Register />
       </header>
     </div>
   );
