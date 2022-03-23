40e675e0294e420dc6778cd1054b867fbbf4bcca
jonandonigv <jonandonigv@gmail.com>
2022-03-23 20:06:24 +0100

    Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
 import { Home } from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
+import Product from "./pages/Product/Product";
+import ProductList from "./pages/ProductList/ProductList";
 import Register from "./pages/Register/Register";
+import {
+  BrowserRouter as Router, Routes, Route, Navigate
+} from 'react-router-dom';
 
 function App() {
+  const user = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <Register />
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={user ? <Home /> : <Navigate to={"register"}/>}/>
+        <Route exact path='/register' element={!user ? <Register /> : <Navigate to={"/"}/>}/>
+        <Route exact path='/login' element={!user ? <Login /> : <Navigate to={'/'} />}/>
+        <Route path="/products/:category" element={<ProductList />}/>
+        <Route path="/product/:id" element={<Product />}/>
+        <Route path='/cart' element={<Cart />}/>
+        <Route path='/success' element={<Success />} />
+      </Routes>
+    </Router>
   );
 }
 
