7b6007197f6f354d6091da976fcfd6bbb96ab00b
jonandonigv <jonandonigv@gmail.com>
2022-04-04 03:05:22 +0200

    Home

--- admin/src/Pages/home/Home.jsx
+++ admin/src/Pages/home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './home.scss';
 import Navbar from '../../Components/Navbar/Navbar';
 import Sidebar from '../../Components/Sidebar/Sidebar';
+import Widget from '../../Components/Widget/Widget';
+import Featured from '../../Components/Featured/Featured';
 
 const Home = () => {
   return (
@@ -9,6 +11,20 @@ const Home = () => {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
+        <div className="widgets">
+          <Widget type="user"/>
+          <Widget type="order"/>
+          <Widget type="earning"/>
+          <Widget type="balance"/>
+        </div>
+        <div className="charts">
+          <Featured />
+          {/* <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} /> */}
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          {/* <Table /> */}
+        </div>
       </div>
     </div>
   )
