46c8563cada0cebbc90e8728d5dc8555774a8c80
jonandonigv <jonandonigv@gmail.com>
2022-04-05 22:33:17 +0200

    List

--- admin/src/Pages/list/List.jsx
+++ admin/src/Pages/list/List.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+// import "./list.scss"
+// import Sidebar from '../../Components/Sidebar/Sidebar';
+// import Navbar from '../../Components/Navbar/Navbar';
+// import Datatable from "../../Components/DataTable/DataTable";
 
-const List = () => {
-  return (
-    <div>List</div>
-  )
-}
+// const List = () => {
+//   return (
+//     <div className="list">
+//       <Sidebar/>
+//       <div className="listContainer">
+//         <Navbar/>
+//         <Datatable/>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default List
+// export default List
